b92df8e4ab8903a03e84e3149cb8b28622b290c8
nhan712 <chinhan712@gmail.com>
2015-09-29 16:44:52 +0700

    Commit common function js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+/*Custom JS*/
+$("header").load( "header.html", function( response, status, xhr ) {
+	if ( status != "error" ) {
+		var url = window.location.href; 
+		if (url.indexOf('screen04') != -1) {
+			$('.page-title').text('部屋情報');
+			getHighLightMenu('menuRoom');
+		}
+		if (url.indexOf('screen08') != -1) 
+			$('.page-title').text('宿泊客検索');
+		if (url.indexOf('screen14') != -1) 
+			$('.page-title').text('ピーク時間情報');
+		if (url.indexOf('screen15') != -1) 
+			$('.page-title').text('インジケータ');
+	}
+});
 
+function getHighLightMenu(_menuName) {
+	/*Common*/
+	var _tabName = getUrlParameter('tab');
+	$('#'+_menuName + _tabName.charAt(0).toUpperCase() + _tabName.slice(1)).addClass("active");
+	$('#'+_menuName).trigger('click'); 
+}
