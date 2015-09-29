00d47fd617001575d4125d97c9f793efcba0af9e
nhan712 <chinhan712@gmail.com>
2015-09-29 17:36:10 +0700

    update

--- js/main.js
+++ js/main.js
@@ -4,23 +4,30 @@ $("header").load( "header.html", function( response, status, xhr ) {
 		var url = window.location.href; 
 		if (url.indexOf('screen04') != -1) {
 			$('.page-title').text('部屋情報');
-			getHighLightMenu('menuRoom', 'screen04');
+			getHighLightMenu('menuRoom');
 		}
-		if (url.indexOf('screen08') != -1) 
+		if (url.indexOf('screen08') != -1) {
 			$('.page-title').text('宿泊客検索');
-		if (url.indexOf('screen14') != -1) 
+			getHighLightMenu('menuSearch');
+		}
+		if (url.indexOf('screen14') != -1) {
 			$('.page-title').text('ピーク時間情報');
-		if (url.indexOf('screen15') != -1) 
+			getHighLightMenu('menuPeak');
+		}
+		if (url.indexOf('screen15') != -1) {
 			$('.page-title').text('インジケータ');
+			getHighLightMenu('menuIndicator');
+		}
 	}
 });
 
-function getHighLightMenu(_menuName, _screenName) {
+function getHighLightMenu(_menuName) {
 	/*Common*/
 	var _tabName = getUrlParameter('tab');
-	if (_screenName == 'screen04') {
-		$('#'+_tabName+'Div').trigger('click'); 
-	}
+	$('#'+_tabName+'Div').trigger('click');
+	// if (_screenName == 'screen04') {
+	// 	$('#'+_tabName+'Div').trigger('click'); 
+	// }
 	$('#'+_menuName + _tabName.charAt(0).toUpperCase() + _tabName.slice(1)).addClass("active");
 	$('#'+_menuName).trigger('click'); 
 }
